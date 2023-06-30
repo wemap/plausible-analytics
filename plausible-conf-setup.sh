06f98d528b6d89f4b250c7d2356a611e5ffedebb
Adrian Ruiz <adrianruizmora@hotmail.com>
2023-06-30 12:07:31 +0200

    feat: fix path setup script

--- plausible-conf-setup.sh
+++ plausible-conf-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 my_ip=$(curl http://checkip.amazonaws.com)
 secret_kb=$(openssl rand -base64 64)
-echo "ADMIN_USER_EMAIL=adrian@getwemap.com" > /var/www/hosting/plausible-conf.env
-echo "ADMIN_USER_NAME=admin" >> /var/www/hosting/plausible-conf.env
-echo "ADMIN_USER_PWD=MKeWSSN3bcEpGCuyny7d1bsJFCZH6r2DhsHiPHuktU=" >> /var/www/hosting/plausible-conf.env
-echo "BASE_URL=http://$my_ip" >> /var/www/hosting/plausible-conf.env
-echo "SECRET_KEY_BASE=$secret_kb" >> /var/www/hosting/plausible-conf.env
+echo "ADMIN_USER_EMAIL=adrian@getwemap.com" > /var/www/plausible-analytics/plausible-conf.env
+echo "ADMIN_USER_NAME=admin" >> /var/www/plausible-analytics/plausible-conf.env
+echo "ADMIN_USER_PWD=MKeWSSN3bcEpGCuyny7d1bsJFCZH6r2DhsHiPHuktU=" >> /var/www/plausible-analytics/plausible-conf.env
+echo "BASE_URL=http://$my_ip" >> /var/www/plausible-analytics/plausible-conf.env
+echo "SECRET_KEY_BASE=$secret_kb" >> /var/www/plausible-analytics/plausible-conf.env
